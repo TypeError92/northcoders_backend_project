56232ce3fe46ed5a066b32f4f5cb0ce86e4f36d2
TypeError92 <132596140+TypeError92@users.noreply.github.com>
2023-08-14 16:26:22 +0100

    add a new endpoint GET /api/articles/:article_id that works for the happy path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const {
+    getArticleById,
     getEndpoints,
     getTopics
 } = require('./controllers')
@@ -9,9 +10,12 @@ const app = express();
 
 app.get('/api', getEndpoints)
 
+app.get('/api/articles/:article_id', getArticleById)
+
 app.get('/api/topics', getTopics)
 
 app.use((err, req, res, next) => {
+    console.log(err)
     res.status(500).send({msg: 'Internal server error'})
 })
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const {getEndpoints} = require('./api')
+const {getArticleById} = require('./api-articles')
 const {getTopics} = require('./api-topics')
 
 module.exports = {
+    getArticleById,
     getEndpoints,
     getTopics
 }
