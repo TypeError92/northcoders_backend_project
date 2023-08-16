49488c9706f9ff46168dfa728673cbf4ecc4a7bd
TypeError92 <132596140+TypeError92@users.noreply.github.com>
2023-08-16 13:17:32 +0100

    add new checkExists util function for models

--- models/comments.models.js
+++ models/comments.models.js
@@ -1,21 +1,26 @@
+const {checkExists} = require('./utils.models')
 const db = require('../db/connection')
 
 function fetchComments(article_id){
-    return db.query(
-        `
-        SELECT
-            comment_id,
-            votes,
-            created_at,
-            author,
-            body,
-            article_id
-        FROM comments
-        WHERE article_id = $1
-        ORDER BY created_at DESC;
-        `,
-        [article_id]
-    )
+    return checkExists('articles', 'article_id', article_id)
+    .then(() => {
+        return db.query(
+            `
+            SELECT
+                comment_id,
+                votes,
+                created_at,
+                author,
+                body,
+                article_id
+            FROM comments
+            WHERE article_id = $1
+            ORDER BY created_at DESC;
+            `,
+            [article_id]
+        )
+    }) 
+    
 }
 
 module.exports = {fetchComments}
